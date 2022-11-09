fe50d46a58a1f95fdbd4b0aac13b0be776dbb023
flisnes <haakonflisnes@gmail.com>
2022-11-09 21:45:30 +0100

    Added functions for multiplication and division

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ fn main() {
 
     println!("q_add: {} -> as float: {}", q_add(pi_q16_16, sqrttwo_q16_16), fixed_to_float(q_add(pi_q16_16, sqrttwo_q16_16), 16));
     println!("q_sub: {} -> as float: {}", q_sub(pi_q16_16, sqrttwo_q16_16), fixed_to_float(q_sub(pi_q16_16, sqrttwo_q16_16), 16));
+    println!("q_mul: {} -> as float: {}", q_mul(pi_q16_16, sqrttwo_q16_16, 16), fixed_to_float(q_mul(pi_q16_16, sqrttwo_q16_16, 16), 16));
+    println!("q_div: {} -> as float: {}", q_div(pi_q16_16, sqrttwo_q16_16, 16), fixed_to_float(q_div(pi_q16_16, sqrttwo_q16_16, 16), 16));
 
 }
 
@@ -56,4 +58,48 @@ fn q_add(a: i32, b: i32) -> i32 {
 
 fn q_sub(a: i32, b: i32) -> i32 {
     return a - b;
+}
+
+fn q_mul(a: i32, b: i32, frac_bits: i8) -> i32 {
+    let result: i32;
+    let mut tmp: i64;
+
+    // Multiply the two factors, the product will have the wrong scaling which we correct later
+    tmp = a as i64 * b as i64;
+
+    // Rounding; mid values are rounded up
+    tmp += (1 << (frac_bits - 1)) as i64;
+
+    // Correct by dividing by base
+    tmp = tmp >> frac_bits;
+
+    // Saturate
+    if tmp > 0x7FFFFFFF {
+        tmp = 0x7FFFFFFF;
+    }
+    if tmp < -1 * 0x80000000 {
+        tmp = 0x80000000;
+    }
+
+    result = tmp as i32;
+    return result;
+}
+
+fn q_div(a: i32, b: i32, frac_bits: i8) -> i32 {
+    let result: i32;
+    let mut tmp: i64;
+
+    // Prescaling up, since division removes scaling
+    tmp = (a as i64) << frac_bits;
+
+    // Rounding up positive numbers and down negative numbers
+    if (tmp >= 0 && b >= 0) || (tmp < 0 && b < 0) {
+        tmp += (b as i64) >> 1;
+    } else {
+        tmp -= (b as i64) >> 1;
+    }
+
+    // Division
+    result = (tmp / b as i64) as i32;
+    return result;
 }
