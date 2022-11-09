743307fb47d802d51d0924bd4542e3efe81910d7
flisnes <haakonflisnes@gmail.com>
2022-11-09 21:42:10 +0100

    Functions for adding Q-notation numbers

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ fn main() {
     println!("Main function start");
 
     let pi: f64 = 3.141592653589793;
+    let sqrttwo: f64 = 1.414213562373095;
 
     let pi_q16_16: i32 = float_to_fixed(pi, 16);
+    let sqrttwo_q16_16: i32 = float_to_fixed(sqrttwo, 16);
 
     let pi_from_q: f64 = fixed_to_float(pi_q16_16, 16);
     println!("fixed: {}, float: {}", pi_q16_16, pi_from_q);
     println!("Conversion error: {}", pi-pi_from_q);
+
+    println!("q_add: {} -> as float: {}", q_add(pi_q16_16, sqrttwo_q16_16), fixed_to_float(q_add(pi_q16_16, sqrttwo_q16_16), 16));
 
 }
 
@@ -30,4 +34,21 @@ fn fixed_to_float(num: i32, frac_bits: i8) -> f64 {
     f = f * sign;
 
     return f;
+}
+
+fn q_add(a: i32, b: i32) -> i32 {
+    let result: i32;
+    let mut tmp: i64;
+    tmp = a as i64 + b as i64;
+
+    // Saturate
+    if tmp > 0x7FFFFFFF {
+        tmp = 0x7FFFFFFF;
+    }
+    if tmp < -1 * 0x80000000 {
+        tmp = 0x80000000;
+    }
+
+    result = tmp as i32;
+    return result;
 }
